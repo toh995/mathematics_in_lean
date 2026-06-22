d70a38cafff2e8993b034ae25e828ff3cf02e1be
toh995 <52012721+toh995@users.noreply.github.com>
2026-06-21 23:08:15 -1000

    2.4 exercises

--- my_files/C02_Basics/S04_More_on_Order_and_Divisibility.lean
+++ my_files/C02_Basics/S04_More_on_Order_and_Divisibility.lean
@@ -39,17 +39,53 @@ example : min a b = min b a := by
     apply min_le_left
 
 example : max a b = max b a := by
-  sorry
+  apply le_antisymm
+  repeat
+    apply max_le
+    apply le_max_right
+    apply le_max_left
+
 example : min (min a b) c = min a (min b c) := by
-  sorry
+  have h0 : min (min a b) c ≤ min a (min b c) := by
+    have ha : min (min a b) c ≤ a := le_trans (min_le_left (min a b) c) (min_le_left a b)
+    have hb : min (min a b) c ≤ b := le_trans (min_le_left (min a b) c) (min_le_right a b)
+    have hc : min (min a b) c ≤ c := min_le_right (min a b) c
+    apply le_min
+    · apply ha
+    · apply le_min hb hc
+
+  have h1 : min a (min b c) ≤ min (min a b) c := by
+    have ha : min a (min b c) ≤ a := min_le_left a (min b c)
+    have hb : min a (min b c) ≤ b := le_trans (min_le_right a (min b c)) (min_le_left b c)
+    have hc : min a (min b c) ≤ c := le_trans (min_le_right a (min b c)) (min_le_right b c)
+    apply le_min
+    · apply le_min ha hb
+    · apply hc
+
+  apply le_antisymm h0 h1
+
+
 theorem aux : min a b + c ≤ min (a + c) (b + c) := by
-  sorry
+  apply le_min
+  · apply add_le_add_left (min_le_left a b)
+  · apply add_le_add_left (min_le_right a b)
+
 example : min a b + c = min (a + c) (b + c) := by
-  sorry
+  apply le_antisymm
+  · apply aux
+  · have h : min (a + c) (b + c) + (-c) ≤ min (a + c + (-c)) (b + c + (-c)) := by
+      apply le_min
+      · apply add_le_add_left (min_le_left (a+c) (b+c))
+      · apply add_le_add_left (min_le_right (a+c) (b+c))
+    repeat rw[add_neg_cancel_right] at h
+    linarith
+
 #check (abs_add_le : ∀ a b : ℝ, |a + b| ≤ |a| + |b|)
 
-example : |a| - |b| ≤ |a - b| :=
-  sorry
+example : |a| - |b| ≤ |a - b| := by
+  have h : |(a - b) + b| ≤ |a - b| + |b| := by apply abs_add_le
+  rw [sub_add_cancel] at h
+  linarith
 end
 
 section
@@ -66,7 +102,17 @@ example : x ∣ x ^ 2 := by
   apply dvd_mul_left
 
 example (h : x ∣ w) : x ∣ y * (x * z) + x ^ 2 + w ^ 2 := by
-  sorry
+  have h0 : x ∣ y * (x * z) := by
+    have h : y * (x * z) = x * (y * z) := by ring
+    rw[h]
+    apply dvd_mul_right
+  have h1 : ∀ n : ℕ, n ∣ n^2 := by
+    intro n
+    apply dvd_mul_left
+  have h2 : x ∣ w^2 := by
+    apply dvd_trans h (h1 w)
+
+  apply dvd_add (dvd_add h0 (h1 x)) h2
 end
 
 section
@@ -78,7 +124,11 @@ variable (m n : ℕ)
 #check (Nat.lcm_zero_left n : Nat.lcm 0 n = 0)
 
 example : Nat.gcd m n = Nat.gcd n m := by
-  sorry
+  have h : ∀ n m : ℕ, Nat.gcd n m ∣ Nat.gcd m n := by
+    intro n m
+    have h1 : Nat.gcd n m ∣ m := Nat.gcd_dvd_right n m
+    have h2 : Nat.gcd n m ∣ n := Nat.gcd_dvd_left n m
+    apply Nat.dvd_gcd h1 h2
+
+  apply Nat.dvd_antisymm (h m n) (h n m)
 end
-
-
